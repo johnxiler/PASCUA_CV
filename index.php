7e7c1263cad425b209bb1b5289e538f31f682e08
John Relix Pascua <65880846+johnxiler@users.noreply.github.com>
2022-02-24 17:03:48 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang= "en">
+  <head>
+    <title>CV</title>
+    
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="description" content="" />
+    
+    <style>
+     *{
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+   font-family: 'Cairo', sans-serif;
+ }
+html,body {
+      width:100%;
+      height:100%;
+      /* color: rgb(163, 235, 163); */
+    }
+
+body{
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#b5bdc8+0,828c95+38,28343b+100 */
+background: #b5bdc8; /* Old browsers */
+background: -moz-linear-gradient(top,  #b5bdc8 0%, #828c95 38%, #28343b 100%); /* FF3.6-15 */
+background: -webkit-linear-gradient(top,  #b5bdc8 0%,#828c95 38%,#28343b 100%); /* Chrome10-25,Safari5.1-6 */
+background: linear-gradient(to bottom,  #b5bdc8 0%,#828c95 38%,#28343b 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#b5bdc8', endColorstr='#28343b',GradientType=0 ); /* IE6-9 */
+
+  
+}
+.title{
+  font-family: 'Nunito', sans-serif;
+  font-weight: 'bold';
+  font-size: 50px;
+}
+ .p{
+    font-style: 'Roboto', sans-serif;
+    -moz-column-count: 1;
+    -moz-column-gap: 20px;
+    -webkit-column-count: 2;
+    -webkit-column-gap: 20px;
+    column-count: 1;
+    column-gap: 20px;
+    }
+
+.card-options {
+   display: flex;
+   flex-direction: row;
+   align-items: stretch;
+   margin-right:auto;
+   margin-left:auto;
+   
+}
+.card1{
+  width: 25vw;
+  height: 90vh;
+  background-color: #D4E4F7;
+  border-radius: 15px;
+  display:grid;
+  grid-template-rows: auto 400px;
+  box-shadow: 0 0 2px white;
+  overflow: hidden;
+  margin-right:55px;
+}
+.card1 #profile{
+  margin-top:25px;
+}
+#name{
+  margin-left:45px;
+  margin-top: 25px;
+  color: #B85B14;
+  font-weight:bold;
+  font-size:25px;
+}
+#email{
+  margin-left:25px;
+  margin-top: 25px;
+  color: #B85B14;
+}
+#address{
+  margin-left:25px;
+  margin-top: 25px;
+  color: #B85B14;
+}
+
+#contact{
+  margin-left:25px;
+  margin-top: 25px;
+  color: #B85B14;
+
+}
+#intro{
+  margin-left:25px;
+  margin-top: 25px;
+  margin-right:25px;
+  color: #B85B14;
+  text-align: justify;
+}
+
+.card2{
+  width: 50vw;
+  height: 90vh;
+  background-color: #AAC743;
+  border-radius: 15px;
+}
+
+.head{
+    font-family: 'Nunito', sans-serif;
+    justify-content:center;
+    display:flex;
+    text-align:justify;
+    margin-top:5px;
+}
+.parag{
+   font-family: 'Nunito', sans-serif;
+   text-align:justify;
+   margin-left:25px;
+   margin-right:25px;
+}
+</style>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,100&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <?php echo "<p class='title'>CURRICULUM VITAE</p>" ?> 
+  <div class="card-options">
+  <div class="card1">
+    <div class="image-box">
+      <img id="profile"style="display: block; border-radius: 100%; margin-left: auto;
+  margin-right: auto;" src="https://i.ibb.co/pQGmtZR/2x2pic.jpg" alt="2x2pic"/>
+    </div>
+    <div>
+     <p>
+     <?
+        echo"<p id='name' style='font-size:big;'>John Relix Pascua</p>";
+        echo"<p id='email' style='font-size:small;'>Email: johnxiler@mail.com</p>";
+        echo "<p id='address' style='font-size:small;'>Address: Lag-on, Daet Camarines Norte</p>";
+        echo "<p id='contact' style='font-size:medium;'>Linkedin:<a href='https://www.linkedin.com/in/johnrelixpascua/'> https://www.linkedin.com/in/johnrelixpascua/</a></p>";
+        echo"<p id='intro'>Solution-focused Python Developer with 2+ years of experience developing web applications using Python, JAVA, and Spring. Instrumental in writing code, implementing Python applications, ensuring data security and protection, and identifying data storage solutions. Expertise in data processing automation using python, machine learning, and multi-process architecture.</p>";
+     ?>
+     </p>
+    </div>
+  </div><!--end of card1-->
+  <div class="card2">
+      <div>
+         <?
+           echo "<h1 class='head'>SKILLS</h1>";
+           echo "<p class='parag'>Hard Skills:</p>"; 
+           echo "<p class='parag'>I have knowledge in different programming language and frameworks such as</p>";  
+           echo "<p class='parag'>Python,Java,Javascript,Solidity for the blockchain development and their famous frameworks typescript,django,springboot,Database,Back End,Scripting,Git,SQL,Unix,Github,Linux,PostgreSQL and mongoDB environment</p>"; 
+           echo "<p class='parag'>Implemented full-stack programming techniques to develop a mission-critical external web app, which increased user retention by 78%.</p>";   
+           echo "<p class='parag'>Soft Skills:</p>";
+           echo "<p class='parag'>I can communicate in english and tagalog, I can work with a team of developers if the company needs it.</p>";  
+                
+         ?>    
+     </div>
+     <div>
+          <?
+           echo "<h1 class='head'>EDUCATIONAL BACKGROUND</h1>";
+           echo "<p class='parag'>Bachelor of Science Major in Information Technology(first entry year 2013)</p>";
+           echo "<p class='parag'>St.Claire Institute of Technology(TechVoc Course)(2011)</p>"; 
+           echo "<p class='parag'>Porfirio R. Ponayo High School(2007-2010)</p>";
+           echo "<p class='parag'>Don Emiliano L. Pabico Elementary School(2003-2007)</p>";   
+         ?> 
+     </div>
+     <div>
+         <?
+           echo "<h1 class='head'>WORK HISTORY</h1>";
+           echo "<p class='parag'>Central Plaza Corporation/&nbspDaet,Camarines Norte/&nbsp(2012)<br>
+                7-Eleven Corp./&nbspDaet,Camarines Norte/&nbsp(2012)<br>
+                Fotograce/&nbspDaet,Camarines Norte/&nbsp(2012)<br> I work here as a sales associates and clerical jobs helping the company to maintain sales and relations to customer.";  
+         ?>     
+    </div>
+     <div>
+          <?
+           echo "<h1 class='head'>ACCOMPLISHMENTS</h1>";
+           echo "<p class='parag'>I have a much more on learning by doing and watching any programming videos than reading resources,informative videos,playing guitars and playing games. </p>"; 
+         ?> 
+     </div>
+   </div>
+  </div><!--end of card1-->
+  </body>
+</html>
